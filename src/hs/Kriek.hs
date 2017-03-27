a9416815e6609038c2bacf3ce5e0194e60e4c955
Antonis Kalou <kalouantonis@gmail.com>
2017-03-27 22:36:12 +0300

    Added simple command line parsing and simple compiler API

--- src/hs/Kriek.hs
+++ src/hs/Kriek.hs
@@ -1,7 +1,26 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Kriek where
 
-import Kriek.Repl (repl)
-import Kriek.Runtime.Data
+import qualified Kriek.Compiler as C
+import qualified Kriek.Repl as R
+import Kriek.Runtime.Data (newState)
+import System.Console.CmdArgs
+
+data KriekCmd = Repl
+              | Compile { src :: FilePath, out :: FilePath }
+              deriving (Data, Typeable)
+
+repl :: KriekCmd
+repl = Repl
+
+compile :: KriekCmd
+compile = Compile
+  { src = def &= argPos 0 &= typ "SRC"
+  , out = def &= argPos 1 &= typ "OUT"}
 
 main :: IO ()
-main = repl newState
+main = do
+  args <- cmdArgs (modes [repl, compile])
+  case args of
+    Repl -> R.repl newState
+    Compile src out -> C.compileFile src out
---
+++ src/hs/Kriek/Compiler.hs
@@ -0,0 +1,20 @@
+module Kriek.Compiler (compile, compileFile) where
+
+import Kriek.Reader (program)
+import Text.Megaparsec (parse, parseErrorPretty)
+import Data.Maybe
+
+-- Accepts an optional file path of the file being compiled.
+--
+-- TODO: Don't use strings
+compile :: Maybe FilePath -> String -> String
+compile path s = case parse program p s of
+                   Left e -> error $ parseErrorPretty e
+                   Right x -> show x
+  where p = fromMaybe "" path
+
+-- Compile file from `src` and store the output in `out`.
+compileFile :: FilePath -> FilePath -> IO ()
+compileFile src out = do
+  contents <- readFile src
+  writeFile out $ compile (Just src) contents
